7f33eb3752e4d480b6147a3defe54efaf9a3fdb7
tech-voyager <161888947+tech-voyager@users.noreply.github.com>
2024-03-08 07:42:26 +0100

    Delete pkg/coding.js

--- pkg/coding.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-const crypt = (value, key) => {
-  var ukey = crypto.createCipher("aes-128-cbc", key);
-  var result = uKey.update(values, "utf8", "hex");
-  result += ukey.final("hex");
-  return result;
-}
-
-module.exports = { crypt };
